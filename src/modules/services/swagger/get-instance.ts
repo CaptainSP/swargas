9a9c8ab043306b3acab320f0aa31899f00ba86ac
CaptainSP <19982002ke@gmail.com>
2024-06-22 02:08:35 +0300

    chore: Refactor getInstance function to use provided type parameter and add missing keys

--- src/modules/services/swagger/get-instance.ts
+++ src/modules/services/swagger/get-instance.ts
@@ -1,6 +1,17 @@
 import { getKeysOfType } from "../../swagger/object-of";
 
 export function getInstance(type: any) {
+
+  if (!type) {
+    return {
+      keys: [],
+      types: {},
+      options: {},
+      valueTypes: {},
+      itemOptions: {},
+    };
+  }
+
   const keys = type.__keys__;
   const types = type.__types__;
   const options = type.__options__;
